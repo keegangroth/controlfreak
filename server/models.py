baf0d250706a27c4ee7e9ca1b07ae61bfb818483
Keegan Roth <keegan.roth@lookout.com>
2020-01-22 20:03:43 -0500

    Add Device model!
    With device id relaation. Also add super basic get routes for all
    devices and device by id.

--- server/models.py
+++ server/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ApplicationModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Device(ApplicationModel):
+    token = models.CharField(max_length=100)
+
+
+class DeviceId(ApplicationModel):
+    IdType = models.TextChoices('IdType', 'GOOGLE_AD_ID IOS_ID')
+    id_type = models.CharField(choices=IdType.choices, max_length=100)
+    value = models.CharField(max_length=100)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='device_ids')
--- server/urls.py
+++ server/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path('health', views.health, name='health'),
+    path('devices', views.devices, name='devices'),
+    path('devices/<int:device_id>', views.device, name='device'),
 ]
