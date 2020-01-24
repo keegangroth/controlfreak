eb822d88e6d8aafaaff2438948348b0bd715152e
Keegan Roth <keegan.roth@lookout.com>
2020-01-24 11:20:51 -0500

    Add associations to device admin page via inlines

--- server/admin.py
+++ server/admin.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Device)
+
+class DeviceIdInline(admin.TabularInline):
+    model = models.DeviceId
+    extra = 0
+    readonly_fields = ('id_type', 'value')
+    can_delete = False
+
+class CredentialInline(admin.TabularInline):
+    model = models.Credential
+    extra = 0
+
+class LogInline(admin.TabularInline):
+    model = models.Log
+    extra = 0
+
+class DeviceAdmin(admin.ModelAdmin):
+    inlines = [
+        DeviceIdInline,
+        CredentialInline,
+        LogInline,
+    ]
+
+
+admin.site.register(models.Device, DeviceAdmin)
 admin.site.register(models.DeviceId)
 admin.site.register(models.Credential)
 admin.site.register(models.Log)
