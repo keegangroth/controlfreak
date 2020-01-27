22dea499459426544ae727a120aa04a9a6355071
Keegan Roth <keegan.roth@lookout.com>
2020-01-27 14:28:14 -0500

    Update ECS service name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,4 +25,4 @@ done
 # Run any DB migrations (async)
 aws ecs run-task --cli-input-json file://$ROOT_DIR/scripts/infrastructure/migration_task.json
 # Force the cluster to run the new code
-aws ecs update-service --cluster controlfreak --service controlfreak-int --force-new-deployment
+aws ecs update-service --cluster controlfreak --service controlfreak --force-new-deployment
