b9854ddd190b06875257e9cf105bcb3ece50236c
Keegan Roth <keegan.roth@lookout.com>
2020-01-29 17:16:38 -0500

    Address djnago deployment suggestions

--- controlfreak/deployed_settings.py
+++ controlfreak/deployed_settings.py
@@ -6,12 +6,15 @@ try:
 except ImportError:
     pass
 
-#DEBUG = False
+DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
 
 SECRET_KEY = json.loads(os.environ['SECRET_KEY'])['SECRET_KEY']
 
 rds_config = json.loads(os.environ['RDS_CREDENTIALS'])
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.' + rds_config['engine'],
